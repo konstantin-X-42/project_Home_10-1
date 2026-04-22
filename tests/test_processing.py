317cf8c19a4a91dc4631392b4496bd8bc7240c12
konstantin <kostyani4y@yandex.ru>
2026-04-22 22:13:38 +0300

    последний тест все проверки проходит

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,5 +1,3 @@
-# import pytest
-
 from src.processing import filter_by_state, sort_by_date
 
 
@@ -56,3 +54,58 @@ def test_not_correct_get_date_executed(date_list_executed):
         != date_list_executed
     )
     assert filter_by_state() != date_list_executed
+
+
+def test_correct_sort_by_date_true(date_list_true):
+    assert (
+        sort_by_date(
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ]
+        )
+        == date_list_true
+    )
+
+
+def test_correct_sort_by_date_false(date_list_false):
+    assert (
+        sort_by_date(
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ],
+            False,
+        )
+        == date_list_false
+    )
+
+
+def test_not_correct_sort_by_date_true(date_list_true):
+    assert (
+        sort_by_date(
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "dat": "2018-06-30T02:08:58.425572"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ]
+        )
+        != date_list_true
+    )
+    assert (
+        sort_by_date(
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "201фига2:08:58.425572"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ]
+        )
+        != date_list_true
+    )
+    assert sort_by_date() != date_list_true
