697d01db9fbf463e03be97282137a183d5404da3
konstantin <kostyani4y@yandex.ru>
2026-04-24 20:36:47 +0300

    прорешена задача 10.2.12

---
+++ practice_10_2/zadacha_1_taxes_10_2_12.py
@@ -0,0 +1,20 @@
+"""
+Задача 1
+создайте проект, используя poetry, создайте структуру проекта и напишите
+для функции calculate_taxes.
+Отправьте код на GitHub.
+Используйте фикстуры, параметризацию и добейтесь 100% code covarge.
+"""
+
+
+def calculate_taxes(prices: list[float], tax_rate: float) -> list[float]:
+    """Функция вычисляет стоимость товаров с учётом налога."""
+    if tax_rate < 0:
+        raise ValueError('Неверный налоговый процент')
+    taxed_prices = []
+    for price in prices:
+        if price <= 0:
+            raise ValueError('Неверная цена')
+        tax = price * tax_rate / 100
+        taxed_prices.append(price + tax)
+    return taxed_prices
