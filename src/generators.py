0814fc9cb3cf8d567c4113012704a585ad032a32
konstantin <kostyani4y@yandex.ru>
2026-04-27 21:54:02 +0300

    добавил модуль генератор

---
+++ src/generators.py
@@ -0,0 +1,11 @@
+# from typing import Any, Iterator
+
+# def filter_by_currency(data: list[dict[str, Any]]) -> Iterator[dict[str, Any]]:
+#     pass
+#
+#
+# def transaction_descriptions(data: list[dict[str, Any]]) -> Iterator[str]:
+#     pass
+
+
+# card_number_generator
