6c224deb9883c2db5e6dc8042cbf714bf635b026
konstantin-X-42 <kostyani4y@gmail.com>
2026-05-05 15:21:59 +0300

    Update live_11_1_02_05_2026.py
    148 тест

--- practice_11_1/live_11_1_02_05_2026.py
+++ practice_11_1/live_11_1_02_05_2026.py
@@ -168,12 +168,12 @@ if __name__ == "__main__":
     def test_gen_chain():  # type: ignore
         lst_t1 = [59, 120]
         lst_t2 = ["l", "i"]
-        gen_iter_1 = gen_chain(lst_t1, lst_t2) # <<< запускаем генератор
+        gen_iter_1 = gen_chain(lst_t1, lst_t2) # <<< вызываем генератор
 
         result_lst = [59, "l", 120, "i"]
         assert list(gen_iter_1) == result_lst
 
-        gen_iter_2 = gen_chain(lst_t1, lst_t2) # <<< запускаем генератор
+        gen_iter_2 = gen_chain(lst_t1, lst_t2) # <<< вызываем генератор
         assert next(gen_iter_2) == result_lst[0]
         assert next(gen_iter_2) == result_lst[1]
         assert next(gen_iter_2) == result_lst[2]
