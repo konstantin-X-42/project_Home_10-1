c91e8b7e227181f6c5085abd3dd821060c60982e
konstantin-X-42 <kostyani4y@gmail.com>
2026-05-22 09:11:42 +0300

    Feature/homework 12 1 (#7)
    * Feature/homework 12 1 practice 12 1 (#6)
    
    * заготовка директории 12.1
    
    * 2-й_commit_12.1
    
    * 12.1.3
    
    * 12/1/5
    
    * 12.1.7
    
    * 12-1-7
    
    * 12-1-5
    
    * 12-1-10
    
    * 12-1-10тест
    
    * 12-1-10
    
    * 12.1.11
    
    * 12.1.11тест
    
    * 12.1.11задача2
    
    * 12.1.11задача2
    
    * 12.1.12
    
    * 12.1.13
    
    * dz
    
    * Update README.md
    
    README
    
    * Update README.md
    
    README
    
    * Update README.md
    
    README
    
    * Update README.md
    
    README
    
    * Update README.md
    
    README
    
    * Update README.md
    
    README
    
    * Update README.md
    
    README
    
    * Update README.md
    
    README
    
    * Update README.md
    
    README
    
    * Update README.md
    
    README
    
    * Update README.md
    
    README
    
    * Update README.md
    
     README
    
    * Update README.md
    
    README_20.05.2026_9:55
    
    * 20.05.2026
    
    * последний на сегодня
    
    * Update README.md
    
    README
    
    * Update README.md
    
    README
    
    * Update README.md
    
    README
    
    * Update README.md
    
    README
    
    * Update README.md
    
    README
    
    * Update README.md
    
    README
    
    * Create test_external_api
    
    создал модуль теста Create test_external_api
    
    * Update test_external_api
    
    test_external_api - заполнен
    
    * Update external_api.py
    
    external_api.py залил
    
    * Create test_utils.py
    
    test_utils создан
    
    * Update test_utils.py
    
    test_utils заполнен
    
    * Delete tests/test_external_api
    
    Delete test_external_api
    
    * Create test_external_api.py
    
    test_external_api создан
    
    * Update test_external_api.py
    
    test_external_api заполнен
    
    * utils готов
    
    * external_api
    
    * поправка
    
    ---------
    
    Co-authored-by: konstantin <kostyani4y@yandex.ru>
    
    * готов на сдачу
    
    ---------
    
    Co-authored-by: konstantin <kostyani4y@yandex.ru>

---
+++ src/utils.py
@@ -0,0 +1,33 @@
+import json
+from pathlib import Path
+from typing import Any
+
+# Абсолютный путь к папке src, где лежит utils.py для файла JSON
+CURRENT_DIR = Path(__file__).resolve().parent
+
+
+def get_transactions(file: str | Path) -> list[dict[str, Any]]:
+    """Читает JSON-файл и возвращает список словарей с транзакциями,
+    файл не найден или пуст или содержит не список, возвращает пустой список.
+    """
+    path = Path(file)
+    if not path.is_file():
+        return []  # если файла по указанному пути не существует - ошибка
+    try:
+        with open(path, "r", encoding="utf-8") as json_file:
+            data = json.load(json_file)
+            if isinstance(data, list):
+                return data
+            return []  # если тип данных не list - ошибка
+    except json.JSONDecodeError, FileNotFoundError:
+        return []  # содержимое файла повреждено или файл отсутствует - ошибка
+
+
+# -----------вызываем функцию------------
+# Относительный путь от корня проекта
+# path_to_file = "../data/operations.json"
+
+# Абсолютный путь к файлу operations.json
+# path_to_file = CURRENT_DIR.parent / "data" / "operations.json"
+#
+# print(get_transactions(path_to_file))
