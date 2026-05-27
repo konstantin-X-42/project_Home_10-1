cfe7d7ab478d976171537e07b23386f2bc722686
konstantin <kostyani4y@yandex.ru>
2026-05-27 22:47:16 +0300

    13.1.7

--- practice_13_1/x_13_1_7_sorting.py
+++ practice_13_1/x_13_1_7_sorting.py
@@ -59,3 +59,60 @@ print(country_df.price.mean())
 # Ukraine                    9.214286
 # Uruguay                   26.403670
 # Name: price, dtype: float64
+
+# ----------------------------------------------------------------------
+print("--   --   --  --   --   --  --   --   --  --   --   --  --   --   --")
+
+# import os
+# import pandas as pd
+
+# 1. Автоматически определяем путь к папке со скриптом
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# 2. Собираем точный путь к Excel-файлу внутри папки data_13_1
+file_path = os.path.join(current_dir, "data_13_1", "winemag-data-130k-v2.xlsx")
+
+# 3. Читаем Excel-файл
+df = pd.read_excel(file_path)
+"""
+код загружает таблицу с данными о винах из Excel-файла, находит самые дорогие и выводит их цены на экран
+"""
+# 4. Сортируем по цене
+df.sort_values(by="price", inplace=True, ascending=False)
+
+# 5. Выводим результат
+print(df.head().price)
+
+# ВОЗВРАЩАЕТ
+# 80290     3300.0
+# 98380     2500.0
+# 15840     2500.0
+# 120391    2013.0
+# 65352     2000.0
+# Name: price, dtype: float64
+
+# ----------------------------------------------------------------------
+print("--   --   --  --   --   --  --   --   --  --   --   --  --   --   --")
+
+# import os
+# import pandas as pd
+
+# Получаем путь к папке, где лежит этот скрипт (data_13_1)
+current_dir = os.path.dirname(os.path.abspath(__file__))
+"""
+код группирует данные по странам и рассчитывает для каждой из них средние показатели цены и рейтинга
+"""
+df = pd.read_excel(os.path.join(current_dir, "data_13_1", "winemag-data-130k-v2.xlsx"))
+
+new_df = df.groupby("country").agg({"price": "mean", "points": "mean"})
+
+print(new_df.head())
+
+# ВОЗВРАЩАЕТ
+#                             price     points
+# country
+# Argentina               24.510117  86.710263
+# Armenia                 14.500000  87.500000
+# Australia               35.437663  88.580507
+# Austria                 30.762772  90.101345
+# Bosnia and Herzegovina  12.500000  86.500000
